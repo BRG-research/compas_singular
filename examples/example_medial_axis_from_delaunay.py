44f88c3e3122fbc38d5fc52f81ea6f1284efee97
Robin Oval <robin.oval@gmail.com>
2017-12-11 17:09:48 +0100

    quad patch decomposition from delaunay without special cases

--- examples/example_medial_axis_from_delaunay.py
+++ examples/example_medial_axis_from_delaunay.py
@@ -4,13 +4,15 @@ from compas.datastructures.mesh import Mesh
 import compas_rhino as rhino
 
 from compas_pattern.topology.medial_axis import medial_axis_from_delaunay
+from compas_pattern.algorithms.delaunay_to_qpd import delaunay_to_qpd
 
 guid = rs.GetObject('get Delaunay mesh')
 mesh = rhino.mesh_from_guid(Mesh, guid)
 
 rs.EnableRedraw(False)
 
-medial_branches = medial_axis_from_delaunay(mesh)
+#medial_branches = medial_axis_from_delaunay(mesh)
+medial_branches = delaunay_to_qpd(mesh)
 
 for branch in medial_branches:
     rs.AddPolyline(branch)
