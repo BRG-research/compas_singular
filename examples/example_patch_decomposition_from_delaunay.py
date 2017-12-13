eaf6c0e767681084b6ae719a79da9427c42f0c78
Robin Oval <robin.oval@gmail.com>
2017-12-13 09:12:35 +0100

    Json files as examples.

--- examples/example_patch_decomposition_from_delaunay.py
+++ examples/example_patch_decomposition_from_delaunay.py
@@ -7,8 +7,10 @@ from compas_pattern.topology.medial_axis import medial_axis_from_delaunay
 from compas_pattern.algorithms.delaunay_to_qpd import delaunay_to_patch_decomposition
 from compas_pattern.topology.mesh_boundary_polylines import mesh_boundaries
 
-guid = rs.GetObject('get Delaunay mesh')
-mesh = rhino.mesh_from_guid(Mesh, guid)
+#guid = rs.GetObject('get Delaunay mesh')
+#mesh = rhino.mesh_from_guid(Mesh, guid)
+
+mesh = Mesh.from_json('rolex_learning_centre.json')
 
 rs.EnableRedraw(False)
 
