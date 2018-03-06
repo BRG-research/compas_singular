bd19e7bc6051e5a2908e7b038e405b05cd2cda3e
Robin Oval <robin.oval@gmail.com>
2018-03-06 10:00:36 +0100

    commenting global_propagation

--- src/compas_pattern/topology/global_propagation.py
+++ src/compas_pattern/topology/global_propagation.py
@@ -17,33 +17,61 @@ __all__ = [
 ]
 
 def face_propagation(mesh, fkey, original_vertices):
+    """Subdivide a polygon face into quads which used to be a quad with four original vertices.
+    Subdivision is valid only if opposite edges have the same number of points or if one only has two.
+    "len(ab) = len(cd) or len(ab) = 2 or len(cd) = 2"
+
+    Parameters
+    ----------
+    mesh : Mesh
+        A quad mesh.
+    fkey: int
+        Key of face to subdivide.
+    original_vertices: list
+        List of four face vertex indices.
+
+    Returns
+    -------
+    mesh : mesh, None
+        The modified quad mesh.
+        None if face was an original quad face with four original vertices or if subdivision if not valid.
+
+    Raises
+    ------
+    -
+
+    """
 
     face_vertices = mesh.face_vertices(fkey)
 
     face_vertex_map = {geometric_key(mesh.vertex_coordinates(vkey)): vkey for vkey in face_vertices}
 
+    # need four original vertices of initial face
     if len(original_vertices) != 4:
         return None
-
     for vkey in original_vertices:
         if vkey not in face_vertices:
             return None
 
+    # sort original vertices
     a, b, c, d = sorted([face_vertices.index(vkey) for vkey in original_vertices])
 
+    # split face vertices per edge
     ab = face_vertices[a : b + 1 - len(face_vertices)]
     bc = face_vertices[b : c + 1 - len(face_vertices)]
     cd = face_vertices[c : d + 1 ]#- len(face_vertices)]
     da = face_vertices[d :] + face_vertices[: a + 1 - len(face_vertices)]
-    #print ab, bc, cd, da
 
+    # check validity for operation
     if len(ab) != len(cd) and len(ab) != 2 and len(cd) != 2:
         return None
     if len(bc) != len(da) and len(bc) != 2 and len(da) != 2:
         return None
 
+    # store information to update potential adjacent faces for further propagation
     update = {}
 
+    # for each pair of opposite edges, get valid input for coons patching with point lists
     if len(ab) == len(cd):
         m = len(ab)
         ab = [mesh.vertex_coordinates(vkey) for vkey in ab]
@@ -79,8 +107,10 @@ def face_propagation(mesh, fkey, original_vertices):
         ad = list(reversed([mesh.vertex_coordinates(vkey) for vkey in da]))
         update[(b, c)] = bc
 
+    # vertices and faces from coons patching of face
     new_vertices, new_face_vertices = discrete_coons_patch(ab, bc, dc, ad)
     
+    # add new vertices only if does not match an existing face
     vertex_remap = []
     for vertex in new_vertices:
         geom_key = geometric_key(vertex)
@@ -91,13 +121,13 @@ def face_propagation(mesh, fkey, original_vertices):
             vkey = mesh.add_vertex(attr_dict = {'x': x, 'y': y, 'z': z})
             vertex_remap.append(vkey)
 
+    # delete old face and add new faces
     mesh.delete_face(fkey)
-
     for face in new_face_vertices:
         mesh.add_face(list(reversed([vertex_remap[vkey] for vkey in face])))
     
+    # update adjacent faces by inserting new vertices
     vertex_map = {geometric_key(mesh.vertex_coordinates(vkey)): vkey for vkey in mesh.vertices()}
-
     for edge, points in update.items():
         u, v = edge
         if u in mesh.halfedge[v] and mesh.halfedge[v][u] is not None:
@@ -107,21 +137,46 @@ def face_propagation(mesh, fkey, original_vertices):
     return mesh
 
 def mesh_propagation(mesh, original_vertices):
+    """Global mesh propagation of local rule/operation that would break the quad constraint.
+
+    Parameters
+    ----------
+    mesh : Mesh
+        A quad mesh.
+    original_vertices: list
+        List of original vertices of the quad mesh before editing.
+
+    Returns
+    -------
+    mesh : mesh
+        The modified quad mesh.
+
+    Raises
+    ------
+    -
+
+    """
 
     count = mesh.number_of_faces()
 
+    # loop over faces and try to propagate
     while count > 0:
         count -= 1
         propagated = False
         for fkey in mesh.faces():
             face_vertices = mesh.face_vertices(fkey)
-            if len(face_vertices) != 4:
+            # if valency higher than 4
+            if len(face_vertices) > 4:
+                # retrieve original vertices
                 face_original_vertices = [vkey for vkey in face_vertices if vkey in original_vertices]
+                # propagate
                 face_propagation(mesh, fkey, face_original_vertices)
                 propagated = True
                 break
+        # if propagation, then try again
         if propagated:
             continue
+        # if no propagation, then stop
         else:
             break
 
