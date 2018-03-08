0d2724fb2775ea1996048bf8c9104bf27bde6f5f
Robin Oval <robin.oval@gmail.com>
2018-03-08 15:30:32 +0100

    visualisation in editing

--- examples/editing.py
+++ examples/editing.py
@@ -27,15 +27,24 @@ mesh = PseudoQuadMesh.from_vertices_and_faces(vertices, face_vertices)
 
 regular_vertices = list(mesh.vertices())
 
-while 1:
-    rules = ['face_pole', 'edge_pole', 'vertex_pole', 'face_opening', 'flat_corner_2', 'flat_corner_3', 'flat_corner_33', 'split_35', 'split_26', 'simple_split', 'double_split', 'insert_pole', 'insert_partial_pole', 'face_strip_collapse', 'face_strip_insert', 'PROPAGATE', 'STOP']
+count = 100
+while count > 0:
+    count -= 1
+    
+    rs.EnableRedraw(False)
+    edges = [rs.AddLine(mesh.vertex_coordinates(u), mesh.vertex_coordinates(v)) for u,v in mesh.edges() if u != v]
+    rs.EnableRedraw(True)
+    
+    rules = ['face_pole', 'edge_pole', 'vertex_pole', 'face_opening', 'flat_corner_2', 'flat_corner_3', 'flat_corner_33', 'split_35', 'split_26', 'simple_split', 'double_split', 'insert_pole', 'insert_partial_pole', 'face_strip_collapse', 'face_strip_insert', 'PROPAGATE', 'DONE']
     rule = rs.GetString('rule?', strings = rules)
     
     if rule == 'PROPAGATE':
         mesh_propagation(mesh, regular_vertices)
-        stop = 0
+        DONE = 0
     else:
-        stop = apply_rule(mesh, rule)
+        DONE = apply_rule(mesh, rule)
+    
+    rs.DeleteObjects(edges)
     
     for vkey in mesh.vertices():
         regular = True
@@ -46,7 +55,7 @@ while 1:
         if regular and vkey not in regular_vertices:
             regular_vertices.append(vkey)
     
-    if stop:
+    if DONE:
         break
 
 mesh_propagation(mesh, regular_vertices)
