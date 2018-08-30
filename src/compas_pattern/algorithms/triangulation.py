8478655f49593b9cf377d3396a2a31592acc4845
Robin Oval <oval@arch.ethz.ch>
2018-08-30 10:48:18 +0200

    fix import

--- src/compas_pattern/algorithms/triangulation.py
+++ src/compas_pattern/algorithms/triangulation.py
@@ -4,7 +4,7 @@ from compas.topology import delaunay_from_points
 
 from compas.utilities import geometric_key
 
-from compas_pattern.topology.unwelding import unweld_mesh_along_edge_path
+from compas_pattern.topology.joining_welding import unweld_mesh_along_edge_path
 
 from compas_pattern.datastructures.mesh import face_circle
 
