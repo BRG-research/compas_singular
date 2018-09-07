c620427c3e17e5ac6208ab6f29a74120ad94c80c
Robin Oval <oval@arch.ethz.ch>
2018-09-07 15:17:57 +0200

    neighbours to neighbors + more general face_strip_insert

--- src/compas_pattern/topology/face_strip_operations.py
+++ src/compas_pattern/topology/face_strip_operations.py
@@ -16,6 +16,8 @@ from compas.geometry import sum_vectors
 
 from compas_pattern.datastructures.mesh import delete_face
 
+from compas.geometry import offset_polyline
+
 __author__     = ['Robin Oval']
 __copyright__  = 'Copyright 2018, Block Research Group - ETH Zurich'
 __license__    = 'MIT License'
@@ -272,7 +274,7 @@ def face_strips_merge(cls, mesh, u, v):
 
     # check
     for vkey in polyedge:
-        if (mesh.is_vertex_on_boundary(vkey) and len(mesh.vertex_neighbours(vkey)) != 3) or (not mesh.is_vertex_on_boundary(vkey) and len(mesh.vertex_neighbours(vkey)) != 4): 
+        if (mesh.is_vertex_on_boundary(vkey) and len(mesh.vertex_neighbors(vkey)) != 3) or (not mesh.is_vertex_on_boundary(vkey) and len(mesh.vertex_neighbors(vkey)) != 4): 
             return None
 
 
@@ -304,7 +306,7 @@ def face_strips_merge(cls, mesh, u, v):
 
     return faces
 
-def face_strip_insert(cls, mesh, vertex_path, factor = .33):
+def face_strip_insert_2(cls, mesh, vertex_path, factor = .33):
 
     # check if vertex_path is a loop
     if vertex_path[0] == vertex_path[-1]:
@@ -326,19 +328,20 @@ def face_strip_insert(cls, mesh, vertex_path, factor = .33):
         for vkey in uv:
             # if was on boundary before unwelding, move along boundary edge
             on_boundary = False
-            for nbr in mesh.vertex_neighbours(vkey):
+            for nbr in mesh.vertex_neighbors(vkey):
                 if mesh.is_edge_on_boundary(vkey, nbr) and nbr not in duplicate_list:
                     x, y, z = mesh.edge_point(vkey, nbr, factor)
                     on_boundary = True
             if not on_boundary:
                 centroids = [mesh.face_centroid(fkey) for fkey in mesh.vertex_faces(vkey)]
                 areas = [mesh.face_area(fkey) for fkey in mesh.vertex_faces(vkey)]
+                #if len(centroids) != 0:
                 x, y, z = sum_vectors([scale_vector(centroid, area / sum(areas)) for centroid, area in zip(centroids, areas)])
-            attr = mesh.vertex[vkey]
-            # factor related to the width of the new face strip compared to the adjacent ones
-            attr['x'] += factor * (x - attr['x'])
-            attr['y'] += factor * (y - attr['y'])
-            attr['z'] += factor * (z - attr['z'])
+                attr = mesh.vertex[vkey]
+                # factor related to the width of the new face strip compared to the adjacent ones
+                attr['x'] += factor * (x - attr['x'])
+                attr['y'] += factor * (y - attr['y'])
+                attr['z'] += factor * (z - attr['z'])
 
     # add new face strip
     for i in range(len(duplicates) - 1):
@@ -357,6 +360,109 @@ def face_strip_insert(cls, mesh, vertex_path, factor = .33):
 
 
     return 0
+
+def face_strip_insert(cls, mesh, vertex_path, pole_extremities, factor = .33):
+
+    # duplicate twice vertices on vertex path
+    duplicated_vertices = {}
+    for vkey in vertex_path:
+        # if closed vertex path, end already included
+        if vkey in duplicated_vertices:
+            continue
+        x, y, z = mesh.vertex_coordinates(vkey)
+        vkey_1 = mesh.add_vertex(attr_dict = {'x': x, 'y': y, 'z': z})
+        vkey_2 = mesh.add_vertex(attr_dict = {'x': x, 'y': y, 'z': z})
+        duplicated_vertices[vkey] = [vkey_1, vkey_2]
+
+    new_faces = []
+
+    # move along edge path
+    for i in range(0, len(vertex_path) - 1):
+        
+        u = vertex_path[i]
+        v = vertex_path[i + 1]
+
+        if v in mesh.halfedge[u]:
+            fkey_left = mesh.halfedge[u][v]
+        else:
+            fkey_left = None
+        if u in mesh.halfedge[v]:
+            fkey_right = mesh.halfedge[v][u]
+        else:
+            fkey_right = None
+
+        # update faces adjacent to vertex path
+        if fkey_left is not None:
+            face_vertices = [duplicated_vertices[vkey][0] if vkey in duplicated_vertices else vkey for vkey in mesh.face_vertices(fkey_left)]
+            mesh.delete_face(fkey_left)
+            mesh.add_face(face_vertices, fkey_left)
+        if fkey_right is not None:
+            face_vertices = [duplicated_vertices[vkey][1] if vkey in duplicated_vertices else vkey for vkey in mesh.face_vertices(fkey_right)]
+            mesh.delete_face(fkey_right)
+            mesh.add_face(face_vertices, fkey_right)
+        
+        # add strip
+        face_vertices = [duplicated_vertices[u][1], duplicated_vertices[v][1], duplicated_vertices[v][0], duplicated_vertices[u][0]]
+        new_faces.append(mesh.add_face(face_vertices))
+
+    # delete old vertices
+    # remove end if closed vertex path
+    if vertex_path[0] == vertex_path[-1]:
+        culled_verted_vertex_path = vertex_path[:-1]
+    else:
+        culled_verted_vertex_path = vertex_path
+    for vkey in culled_verted_vertex_path:
+        if len(mesh.vertex_faces(vkey)) > 0:
+            faces = [mesh.halfedge[vkey][a] for a in mesh.vertex_neighbors(vkey) if mesh.halfedge[vkey][a] is not None]
+            count = len(faces) * 2
+            while len(faces) > 0 and count > 0:
+                for fkey in faces:
+                    for fkey_2 in [fkey_3 for vkey_2 in mesh.face_vertices(fkey) for fkey_3 in mesh.vertex_faces(vkey_2)]:
+                        if duplicated_vertices[vkey][0] in mesh.face_vertices(fkey_2):
+                            duplicate = duplicated_vertices[vkey][0]
+                            face_vertices = [duplicate if key == vkey else key for key in mesh.face_vertices(fkey)]
+                            mesh.delete_face(fkey)
+                            mesh.add_face(face_vertices, fkey)
+                            faces.remove(fkey)
+                            break
+                        if duplicated_vertices[vkey][1] in mesh.face_vertices(fkey_2):
+                            duplicate = duplicated_vertices[vkey][1]
+                            face_vertices = [duplicate if key == vkey else key for key in mesh.face_vertices(fkey)]
+                            mesh.delete_face(fkey)
+                            mesh.add_face(face_vertices, fkey)
+                            faces.remove(fkey)
+                            break
+                    if fkey not in faces:
+                        break
+        mesh.delete_vertex(vkey)
+
+    # transform pole extemities
+    for vkey, is_pole in zip([vertex_path[0], vertex_path[-1]], pole_extremities):
+        if is_pole:
+            u, v = duplicated_vertices[vkey]
+            for fkey in mesh.vertex_faces(v):
+                if fkey is not None:
+                    new_face_vertices = [u if vkey_2 == v else vkey_2 for vkey_2 in mesh.face_vertices(fkey)]
+                    mesh.delete_face(fkey)
+                    mesh.add_face(new_face_vertices, fkey)
+            duplicated_vertices[vkey].remove(v)
+            mesh.delete_vertex(v)
+
+    # move duplicated superimposed vertices towards neighbours' centroid
+    for vkey in [vkey for vkeys in duplicated_vertices.values() for vkey in vkeys]:
+        #if mesh.is_vertex_on_boundary(vkey):
+        #    continue
+        if len(mesh.vertex_neighbors(vkey)) == 0:
+            continue
+        centroids = [mesh.face_centroid(fkey) for fkey in mesh.vertex_faces(vkey)]
+        areas = [mesh.face_area(fkey) for fkey in mesh.vertex_faces(vkey)]
+        x, y, z = sum_vectors([scale_vector(centroid, area / sum(areas)) for centroid, area in zip(centroids, areas)])
+        attr = mesh.vertex[vkey]
+        attr['x'] += factor * (x - attr['x'])
+        attr['y'] += factor * (y - attr['y'])
+        attr['z'] += factor * (z - attr['z'])
+
+    return new_faces
 
 # ==============================================================================
 # Main
