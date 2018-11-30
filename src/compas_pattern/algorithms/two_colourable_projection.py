6863e3f251208aebc28ae5ac12fa0fde5a7757da
Robin Oval <oval@arch.ethz.ch>
2018-11-30 17:40:23 +0100

    re-scripted two-colorable-projection and shifted secondary functions

--- src/compas_pattern/algorithms/two_colourable_projection.py
+++ src/compas_pattern/algorithms/two_colourable_projection.py
@@ -1,17 +1,11 @@
 import itertools
-import copy
 
-from compas_pattern.datastructures.mesh import Mesh
-from compas_pattern.datastructures.mesh import mesh_euler
+from compas_pattern.datastructures.mesh_quad import QuadMesh
 
-from compas.datastructures.network import Network
-from compas_pattern.datastructures.network import graph_disjointed_parts
+from compas_pattern.topology.colorability import is_network_two_colourable
+from compas_pattern.topology.grammar import delete_strip
 
-from compas_pattern.topology.polyline_extraction import mesh_boundaries
-
-from compas.geometry import centroid_points
-
-from compas_pattern.topology.face_strip_operations import face_strip_subdivide
+from compas_pattern.utilities.lists import is_sublist_in_list
 
 __author__     = ['Robin Oval']
 __copyright__  = 'Copyright 2018, Block Research Group - ETH Zurich'
@@ -19,179 +13,13 @@ __license__    = 'MIT License'
 __email__      = 'oval@arch.ethz.ch'
 
 __all__ = [
-	'graph_colourability',
-	'mesh_copy_with_edge_attributes',
-	'graph_from_strip_overlap',
-	'is_graph_two_colourable',
-	'graph_mutiple_vertex_deletion',
-	'mesh_multiple_strip_collapse',
-	'have_meshes_same_euler'
 	'two_colourable_projection',
 ]
 
-def graph_colourability(graph):
-	"""Compute vertex colourability of a graph.
-
-	Parameters
-	----------
-	graph : Network
-		A graph.
-
-	Returns
-	-------
-	int
-		Colourability.
-
-	"""
-
-	return max(list(vertex_coloring(graph.adjacency).values())) + 1
-
-def mesh_copy_with_edge_attributes(mesh):
-
-	edge_attributes = {(u, v): {attr: mesh.get_edge_attribute((u, v), attr) for attr in mesh.edge[u][v]} for u, v in mesh.edges()}
-
-	copy_mesh = mesh.copy()
-	copy_mesh.update_default_edge_attributes()
-	for edge in copy_mesh.edges():
-		copy_mesh.set_edge_attributes(edge, edge_attributes[edge])
-
-	return copy_mesh
-
-def graph_from_strip_overlap(mesh):
-	"""Create the strip overlap graph of a (coarse) quad mesh.
-	A graph node is a quad face strip and a graph edge is a face crossed by two quad face strips.
-	The strip of each edge must be previously stored as an edge attribute.
-
-	Parameters
-	----------
-	mesh : Mesh
-		A (coarse) quad mesh.
-
-	Returns
-	-------
-	graph : graph
-		The strip overlap graph.
-
-	"""
-
-	graph = Network()
-
-	# add vertices
-	strips_to_faces = strip_to_faces_dict(mesh)
-
-	for strip, faces in strips_to_faces.items():
-		if strip == -1:
-			continue
-		x, y, z = centroid_points([mesh.face_centroid(fkey) for fkey in faces])
-		graph.add_vertex(key = strip, attr_dict = {'x': x, 'y': y, 'z': z})
-
-	# add edges
-	edges = []
-	faces_to_strips = faces_to_strips_dict(mesh)
-	for strip_1, strip_2 in faces_to_strips.values():
-		# no duplicates
-		if (strip_1, strip_2) not in edges and (strip_2, strip_1) not in edges:
-			edges.append((strip_1, strip_2))
-	for u, v in edges:
-			graph.add_edge(u, v)
-
-	return graph
-
-def is_graph_two_colourable(graph):
-	"""Try to colour a graph with two colours only.
-
-	Parameters
-	----------
-	graph : Network
-		A graph.
-
-	Returns
-	-------
-	key_to_colour : dict, None
-		A dictionary with vertex keys pointing to colours.
-		None if the graph is not two-colourable.
-
-	"""
-
-	two_colourable = True
-
-	# store colour per vertex key and vertex keys per rank of propagation
-	key_to_colour = {}
-	rank_to_keys = {}
-	current_colour = 0
-	current_rank = 0
-	unvisited = [key for key in graph.vertices() if len(graph.vertex_neighbors(key)) > 0]
-	# case of isolated vertices
-	isolated_vertices = [key for key in graph.vertices() if len(graph.vertex_neighbors(key)) == 0]
-	key_to_colour.update({key: 1 - current_colour for key in isolated_vertices})
-	rank_to_keys[-1] = isolated_vertices
-
-	# start from one of the vertices with the highest valency
-	key_to_degree = {key: len(graph.vertex_neighbors(key)) for key in unvisited}
-	univisted = sorted(unvisited, key=lambda key: key_to_degree[key])
-	key_0 = unvisited.pop()
-	key_to_colour[key_0] = current_colour
-	rank_to_keys[current_rank] = [key_0]
-
-	# loop until all vertices are visited
-	count = len(unvisited)
-	while len(unvisited) > 0 and count > 0:
-		count -= 1
-		# next rank and switch colour
-		current_rank += 1
-		current_colour = 1 - current_colour
-		current_keys = []
-		# collect valid neighbours of previous rank to form next rank
-		for key in rank_to_keys[current_rank - 1]:
-			for nbr in graph.vertex_neighbors(key):
-				if nbr in unvisited and nbr not in current_keys:
-					current_keys.append(nbr)
-		#if no current keys (if graph has disconnected parts) start from a new one
-		if len(current_keys) == 0:
-			current_keys = [unvisited[-1]]
-		# check that vertices of the next rank are not neighbours
-		for key in current_keys:
-			for key_2 in current_keys:
-				if key < key_2:
-					if key_2 in graph.vertex_neighbors(key):
-								two_colourable = False
-		# return None if not two colourable
-		if not two_colourable:
-			return None
-		# store information before continuing
-		else:
-			for key in current_keys:
-				key_to_colour[key] = current_colour
-				unvisited.remove(key)
-			rank_to_keys[current_rank] = current_keys
-
-	return key_to_colour
-
-def graph_mutiple_vertex_deletion(graph, vertices):
-	# delete multiple nodes of a graph at once
-
-	for vkey in vertices:
-		graph.delete_vertex(vkey)
-
-	return graph
-
 def mesh_multiple_strip_collapse(cls, mesh, strips_to_collapse):
 
-	boundary_vertices = mesh.vertices_on_boundary()
-	boundary_corner_vertices = [vkey for vkey in boundary_vertices if len(mesh.vertex_neighbors(vkey)) == 2]
 
-	edges_to_strips = edges_to_strips_dict(mesh)
 	edges_to_collapse = [edge for edge, strip in edges_to_strips.items() if strip in strips_to_collapse]
-	faces_to_collapse = [fkey for fkey, strips in faces_to_strips_dict(mesh).items() if strips[0] in strips_to_collapse or strips[1] in strips_to_collapse]
-
-	# get groups of vertices to merge via graph of edges to collapse
-	graph = Network()
-	for vkey in mesh.vertices():
-		x, y, z = mesh.vertex_coordinates(vkey)
-		graph.add_vertex(key = vkey, attr_dict = {'x': x, 'y': y, 'z': z})
-	for u, v in edges_to_collapse:
-		graph.add_edge(u, v)
-	parts = graph_disjointed_parts(graph)
 
 	# refine boundaries to avoid collapse
 	to_subdivide = []
@@ -222,160 +50,74 @@ def mesh_multiple_strip_collapse(cls, mesh, strips_to_collapse):
 						if (u, v) not in to_subdivide and (v, u) not in to_subdivide:
 							to_subdivide.append((u, v))
 
-	# delete faces
-	for fkey in faces_to_collapse:
-		mesh.delete_face(fkey)
 
-	for u, v in to_subdivide:
-		face_strip_subdivide(cls, mesh, u, v)
-
-	# merge vertices
-	vertices_to_change = {}
-	for part in parts:
-		if len(part) > 1:
-			# merge at the location of the unique two-valent boundary vertex ...
-			xyz = None
-			for vkey in part:
-				if vkey in boundary_corner_vertices and xyz is None:
-					xyz = mesh.vertex_coordinates(vkey)
-				elif vkey in boundary_corner_vertices and xyz is not None:
-					xyz = None
-					break
-			# ... or the unique boundary vertex ...
-			if xyz is None:
-				for vkey in part:
-					if vkey in boundary_vertices and xyz is None:
-						xyz = mesh.vertex_coordinates(vkey)
-					elif vkey in boundary_vertices and xyz is not None:
-						xyz = None
-						break
-			# ... or at the centroid
-			if xyz is None:
-				xyz = centroid_points([mesh.vertex_coordinates(vkey) for vkey in part])
-			x, y, z = xyz
-			new_vkey = mesh.add_vertex(attr_dict = {'x': x, 'y': y, 'z': z})
-			vertices_to_change.update({vkey: new_vkey for vkey in part})
-
-	for fkey in list(mesh.faces()):
-		face_vertices = [vertices_to_change[vkey] if vkey in vertices_to_change else vkey for vkey in mesh.face_vertices(fkey)]
-		mesh.delete_face(fkey)
-		mesh.add_face(face_vertices, fkey)
-
-	mesh.cull_vertices()
-
-	return mesh
-
-def have_meshes_same_euler(mesh_1, mesh_2):
-	# check if two meshes are manfiold and have the same Euler characteristic
-
-	if mesh_1.is_manifold_corrected() and mesh_2.is_manifold_corrected():
-		if mesh_euler(mesh_1) == mesh_euler(mesh_2):
-
-			return True
-
-	return False
-
-def is_subset_in_set(small_set, big_set):
-	for i in small_set:
-		is_in = False
-		for j in big_set:
-			if i == j:
-				is_in = True
-				break
-		if not is_in:
-			return False
-	return True
-
-def two_colourable_projection(cls, mesh, kmax = 1):
-	"""Project a (coarse) quad mesh on the closest two-colourable sub-spaces.
+def two_colourable_projection(mesh, kmax = 1):
+	"""Projection of a coarse quad mesh to the closest two-colourable sub-spaces.
 
 	Parameters
 	----------
-	mesh : Mesh
-		A (coarse) quad mesh.
+	mesh : CoarseQuadMesh
+		A coarse quad mesh.
 
 	Returns
 	-------
 	two_col_meshes : dict
 		The closest two-colourable meshes per distance: {k: two_col_meshes_k}.
 
+	References
+	----------
+	.. [1] Oval et al., *Topology Finding of Two-Colourable Patterns*. In progress.
+
 	"""
 
-	mesh.cull_vertices()
+	# result for input mesh
+	strip_network = mesh.strip_connectivity()
+	results = {(): is_network_two_colourable(strip_network)}
 
-	store_strip_as_edge_attribute(mesh)
-	strips = strips_to_edges_dict(mesh).keys()
-	strips = [x for x in strips if x is not None]
-	
-	n = len(strips)
-
+	# guarantee valid kmax
+	n = mesh.number_of_strips()
 	if kmax < 1 or kmax > n:
 		kmax = n
-
-	graph = graph_from_strip_overlap(mesh)
-
-	if is_graph_two_colourable(graph):
-		return mesh
-
-	two_colourable_meshes = {}
-	kill = []
-	results = {}
-
+	
+	# start iteration
 	k = 0
 	while k < kmax:
 		k += 1
-		two_colourable_meshes[k] = []
-		# total, good, neutral, bad
-		results[k] = [0,0,0,0]
 
-		for combi in list(itertools.combinations(range(n), k)):
+		# test all combinations of (n k) strips
+		for combination in list(itertools.combinations(list(mesh.strips()), k)):
 
-			kill_too = False
-			for kill_combi in kill:
-				if is_subset_in_set(kill_combi, combi):
-					kill_too = True
-					break
-			if kill_too:
+			# check results from potential previous sub-combinations
+			for previous_combination in results:
+				if is_sublist_in_list(combination, previous_combination):
+					# if a sub-combination yielded an invalid topology do not pursue
+					if result == 'invalid topology':
+						results[combination] = 'already invalid topology'
+						break
+			if combination in results:
 				continue
+			
+			# delete strips in mesh and check validity
+			copy_mesh = mesh.copy()
+			copy_mesh.collect_strips()
+			for skey in combination:
+				delete_strip(copy_mesh, skey)
+			topological_validity = copy_mesh.is_manifold() and copy_mesh.euler() == mesh.euler()
+			if not topological_validity:
+				results[combination] = 'invalid topology'
 
-			results[k][0] += 1
-			trimmed_mesh = mesh_copy_with_edge_attributes(mesh)
-			mesh_multiple_strip_collapse(cls, trimmed_mesh, list(combi))
-			mesh_criteria = have_meshes_same_euler(mesh, trimmed_mesh)
-
-			trimmed_graph = graph.copy()
-			graph_mutiple_vertex_deletion(trimmed_graph, list(combi))
-			graph_criteria = is_graph_two_colourable(trimmed_graph)
-
-			if mesh_criteria and graph_criteria is not None:
-				two_colourable_meshes[k].append([trimmed_mesh, trimmed_graph, graph_criteria])
-				kill.append(combi)
-				results[k][1] += 1
-
-			elif not mesh_criteria:
-				kill.append(combi)
-				results[k][3] += 1
-
+			# delete strip vertices in network and check colourability
 			else:
-				results[k][2] += 1
+				copy_network = strip_network.copy()
+				for vkey in combination:
+					copy_network.delete_vertex(vkey)
+				two_colourability = is_network_two_colourable(copy_network)
+				if not two_colourability:
+					results[combination] = 'not two-colourable'
+				else:
+					results[combination] = [copy_mesh, copy_network, two_colourability]
 
-
-		if results[k][2] == 0:
-			break
-
-
-	return two_colourable_meshes, results
-
-def binom_coeff(n, k):
-	k = min(k, n - k)
-	x = 1
-	y = 1
-	i = n - k + 1
-	while i <= n:
-		x = (x * i) // y
-		y += 1
-		i += 1
-	return x
+	return results
 
 # ==============================================================================
 # Main
@@ -392,12 +134,9 @@ if __name__ == '__main__':
 	faces_1 = [[16, 17, 14, 13], [14, 17, 19, 22], [21, 22, 19, 18], [21, 18, 16, 13], [8, 9, 6, 5], [6, 9, 11, 14], [13, 14, 11, 10], [13, 10, 8, 5], [4, 5, 1, 0], [5, 6, 2, 1], [6, 7, 3, 2], [14, 15, 7, 6], [22, 23, 15, 14], [12, 13, 5, 4], [20, 21, 13, 12], [26, 27, 23, 22], [25, 26, 22, 21], [24, 25, 21, 20]]
 
 
-	mesh = Mesh.from_vertices_and_faces(vertices, faces)
-	n = store_strip_as_edge_attribute(mesh)
-	kmax = 2
-	two_colourable_meshes, tested = two_colourable_projection(Mesh, mesh, kmax = kmax)
+	mesh = QuadMesh.from_vertices_and_faces(vertices, faces)
+	
+	mesh.collect_strips()
+	two_colourable_projection(mesh, kmax = 2)
 
-
-	for k in range(1, kmax + 1):
-		print k, binom_coeff(n, k), len(two_colourable_meshes[k]), tested[k]
 
