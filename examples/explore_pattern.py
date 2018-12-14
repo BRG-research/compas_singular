2a24e314a162135a1aea4d5a841a33163c29277e
Robin Oval <oval@arch.ethz.ch>
2018-12-14 11:55:50 +0100

    Rhino IronPython scripts

---
+++ examples/explore_pattern.py
@@ -0,0 +1,3 @@
+from compas_pattern.cad.rhino.editing import explore_pattern
+
+explore_pattern()
---
+++ examples/skeleton_based_decomposition.py
@@ -0,0 +1,29 @@
+import rhinoscriptsyntax as rs
+
+from compas_pattern.cad.rhino.objects.surface import RhinoSurface
+
+from compas_pattern.algorithms.skeletonisation import surface_skeleton
+from compas_pattern.algorithms.skeletonisation import surface_skeleton_decomposition_mesh
+from compas_pattern.algorithms.skeletonisation import surface_skeleton_decomposition_nurbs
+
+from compas_pattern.algorithms.smoothing import surface_constrained_smoothing
+
+from compas_pattern.cad.rhino.utilities import draw_mesh
+
+precision = .2
+density = .5
+
+srf = RhinoSurface.from_selection()
+
+rs.EnableRedraw(False)
+
+#srfs = surface_skeleton_decomposition_nurbs(srf.guid, precision)
+
+mesh = surface_skeleton_decomposition_mesh(srf.guid, precision)
+mesh.init_strip_density()
+mesh.set_strips_density_target(density)
+mesh.densification()
+surface_constrained_smoothing(mesh.quad_mesh, srf, algorithm = 'area')
+quad_mesh_guid = draw_mesh(mesh.quad_mesh)
+
+rs.EnableRedraw(True)
