fa58a083d0c07f88588a1552bf834b6a979dde82
Robin Oval <oval@arch.ethz.ch>
2018-12-31 15:31:32 +0100

    not much

--- examples/strip_colouring.py
+++ examples/strip_colouring.py
@@ -12,13 +12,13 @@ guid = rs.GetObject('get quad mesh')
 mesh = QuadMesh.from_vertices_and_faces(*RhinoMesh.from_guid(guid).get_vertices_and_faces())
 mesh.collect_strips()
 
-## draw strip connectivity graph, with colours if two colourable
-#strip_connectivity = mesh.strip_connectivity()
-#key_to_group = is_network_two_colourable(strip_connectivity)
-#if key_to_group is not None:
-#    group_to_colour = {0: [255,0,0], 1: [0,0,255], 2: [0,255,0]}
-#    key_to_colour = {key: group_to_colour[group] for key, group in key_to_group.items()}
-#    draw_graph(strip_connectivity, key_to_colour)
+# draw strip connectivity graph, with colours if two colourable
+strip_connectivity = mesh.strip_connectivity()
+key_to_group = is_network_two_colourable(strip_connectivity)
+if key_to_group is not None:
+    group_to_colour = {0: [255,0,0], 1: [0,0,255], 2: [0,255,0]}
+    key_to_colour = {key: group_to_colour[group] for key, group in key_to_group.items()}
+    draw_graph(strip_connectivity, key_to_colour)
 
 ## get coarse quad mesh, draw strip connectivity graph, with colours if two-colourable and draw mesh
 #coarse_mesh = CoarseQuadMesh.from_quad_mesh(mesh)
@@ -42,7 +42,7 @@ if strip_colours is not None:
             contour_2 = rs.AddPolyline(contours[1])
             section = rs.AddLine(contours[0][0], contours[1][0])
             rs.AddObjectsToGroup([contour_1, contour_2], rs.AddGroup())
-            sweep = rs.AddSweep2([contour_1, contour_2], [section])
-            rs.ObjectColor([contour_1, contour_2, sweep], colours[i])
+            #sweep = rs.AddSweep2([contour_1, contour_2], [section])
+            rs.ObjectColor([contour_1, contour_2], colours[i])
             rs.DeleteObject(section)
     rs.EnableRedraw(True)
