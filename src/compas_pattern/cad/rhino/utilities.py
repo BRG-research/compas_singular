5db9ad74a7d8bcf51ecb557563c098ba66d8ff46
Robin Oval <oval@arch.ethz.ch>
2019-01-02 17:38:36 +0100

    rhino utility mesh_move_vertices and arrange_guids in_circle

--- src/compas_pattern/cad/rhino/utilities.py
+++ src/compas_pattern/cad/rhino/utilities.py
@@ -1,3 +1,8 @@
+from math import pi
+from math import cos
+from math import sin
+from math import tan
+
 import compas
 
 try:
@@ -10,6 +15,10 @@ import compas_rhino as rhino
 
 from compas.geometry import bounding_box
 from compas.geometry import distance_point_point
+from compas.geometry import subtract_vectors
+from compas.geometry import centroid_points
+
+from compas_rhino.modifiers import VertexModifier
 
 __author__     = ['Robin Oval']
 __copyright__  = 'Copyright 2017, Block Research Group - ETH Zurich'
@@ -20,6 +29,8 @@ __email__      = 'oval@arch.ethz.ch'
 __all__ = [
 	'draw_mesh',
 	'draw_graph',
+	'arrange_guids_in_circle',
+	'mesh_move_vertices'
 ]
 
 def draw_mesh(mesh, group = True):
@@ -45,10 +56,7 @@ def draw_mesh(mesh, group = True):
 	for fkey in mesh.faces():
 		
 		if len(mesh.face_vertices(fkey)) > 4:
-			#return edges
-			rs.EnableRedraw(False)
 			edges =  [rs.AddLine(mesh.vertex_coordinates(u), mesh.vertex_coordinates(v)) for u, v in mesh.edges() if mesh.vertex_coordinates(u) != mesh.vertex_coordinates(v)]
-			rs.EnableRedraw(True)
 			if group:
 				rs.AddObjectsToGroup(edges, rs.AddGroup())
 			return edges
@@ -76,8 +84,6 @@ def draw_graph(graph, key_to_colour = {}):
 
 	"""
 
-	rs.EnableRedraw(False)
-
 	# scale for the size of the vertices
 	box = bounding_box([graph.vertex_coordinates(vkey) for vkey in graph.vertices()])
 	scale = distance_point_point(box[0], box[6])
@@ -97,9 +103,67 @@ def draw_graph(graph, key_to_colour = {}):
 	group = rs.AddGroup()
 	rs.AddObjectsToGroup(spheres + lines, group)
 
-	rs.EnableRedraw(True)
-
 	return group
+
+def arrange_guids_in_circle(guids, centre = None, radius = None):
+	"""Arrange Rhino objects along a circle.
+
+	Parameters
+	----------
+	guids : list
+		The list of Rhino objects to arrange.
+	centre : list
+		The XYZ-coordinates of the centre of the circle. If None, the centroid of the Rhino objects.
+	radius : float
+		The radius of the circle. If None, a radius is computed based on the max bounding box of the Rhino objects.
+
+	Returns
+	-------
+	guid_xyz : dict
+		A dictionary of object guids pointing to new location centre.
+
+	"""
+
+	n = len(guids)
+
+	guid_boxes = {guid: rs.BoundingBox(guid) for guid in guids}
+	guid_xyz0 = {guid: centroid_points([point for point in guid_boxes[guid]]) for guid in guids}
+	if centre is None:
+		centre = centroid_points(guid_xyz0.values())
+
+
+	if radius is None:
+		scales = []
+		for guid in guids:
+			box = guid_boxes[guid]
+			dx = distance_point_point(box[0], box[1])
+			dy = distance_point_point(box[0], box[3])
+			scales.append((dx**2 + dy**2)**.5)
+		scale = max(scales)
+		radius = n * scale / (2 * pi)
+
+	guid_xyz = {}
+
+	for i, guid in enumerate(guids):
+		xyz0 = guid_xyz0[guid]
+		xyz = [centre[0] + radius * cos(2 * pi * float(i) / n), centre[1] + radius * sin(2 * pi * float(i) / n), centre[2]]
+		rs.MoveObject(guid, subtract_vectors(xyz, xyz0))
+		guid_xyz[guid] = xyz
+
+	return guid_xyz
+
+def mesh_move_vertices(mesh, keys):
+	"""Move on vertices of the mesh.
+
+	Parameters
+	----------
+	mesh : Mesh
+		A mesh object.
+	keys : list
+		The list of vertices to move.
+
+	"""
+	return VertexModifier.move_vertices(mesh, keys)
 
 # ==============================================================================
 # Main
