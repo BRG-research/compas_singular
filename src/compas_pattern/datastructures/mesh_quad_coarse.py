102f0681afb0f514899c57dc261872f9a338eacf
Robin Oval <oval@arch.ethz.ch>
2019-02-11 13:29:05 +0100

    fix import

--- src/compas_pattern/datastructures/mesh_quad_coarse.py
+++ src/compas_pattern/datastructures/mesh_quad_coarse.py
@@ -8,11 +8,11 @@ from compas.geometry import Polyline
 
 from compas.utilities import geometric_key
 
-from compas.datastructures.mesh.operations.weld import meshes_join_and_weld
+from compas.datastructures.mesh import meshes_join_and_weld
 from compas_pattern.topology.joining_welding import mesh_unweld_edges
-from compas.datastructures.mesh.operations import mesh_weld
+from compas.datastructures.mesh import mesh_weld
 
-from compas.geometry.algorithms.interpolation import discrete_coons_patch
+from compas.geometry import discrete_coons_patch
 
 from compas.topology import connected_components
 
@@ -374,7 +374,7 @@ def meshes_join_and_weld(meshes, precision = None, cls = None, data = False):
 if __name__ == '__main__':
 
 	import compas
-	from compas.geometry import mesh_smooth_centroid
+	from compas.datastructures.mesh import mesh_smooth_centroid
 	from compas.plotters import MeshPlotter
 
 	vertices = [[12.97441577911377, 24.33094596862793, 0.0], [18.310085296630859, 8.467333793640137, 0.0], [30.052173614501953, 18.846050262451172, 0.0], [17.135400772094727, 16.750551223754883, 0.0], [16.661802291870117, 22.973459243774414, 0.0], [14.180665969848633, 26.949295043945313, 0.0], [36.052761077880859, 26.372636795043945, 0.0], [26.180931091308594, 21.778648376464844, 0.0], [19.647378921508789, 12.288106918334961, 0.0], [9.355668067932129, 16.475896835327148, 0.0], [18.929227828979492, 16.271940231323242, 0.0], [7.34525203704834, 12.111981391906738, 0.0], [13.31309986114502, 14.699410438537598, 0.0], [18.699434280395508, 19.613750457763672, 0.0], [11.913931846618652, 10.593378067016602, 0.0], [17.163223266601563, 26.870658874511719, 0.0], [26.110898971557617, 26.634754180908203, 0.0], [22.851469039916992, 9.81414794921875, 0.0], [21.051292419433594, 7.556171894073486, 0.0], [22.1370792388916, 19.089054107666016, 0.0]]
