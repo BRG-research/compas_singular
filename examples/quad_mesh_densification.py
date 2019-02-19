71c89b550e0e461108605a32c1b14e5ca335c959
Robin Oval <oval@arch.ethz.ch>
2019-02-19 11:57:38 +0100

    not much

--- examples/quad_mesh_densification.py
+++ examples/quad_mesh_densification.py
@@ -12,11 +12,13 @@ from compas.geometry import distance_point_point
 from compas_pattern.cad.rhino.utilities import draw_mesh
 from compas_pattern.cad.rhino.utilities import draw_graph
 
-guid = rs.GetObject('get quad mesh')
-mesh = CoarseQuadMesh.from_vertices_and_faces(*RhinoMesh.from_guid(guid).get_vertices_and_faces())
+guids = rs.GetObjects('get quad meshes')
 
-mesh.collect_strips()
-mesh.init_strip_density()
-mesh.set_strips_density_target(1)
-mesh.densification()
-draw_mesh(mesh.quadmesh)
+for guid in guids:
+    mesh = CoarseQuadMesh.from_vertices_and_faces(*RhinoMesh.from_guid(guid).get_vertices_and_faces())
+    
+    mesh.collect_strips()
+    mesh.init_strip_density()
+    mesh.set_strips_density_target(.5)
+    mesh.densification()
+    draw_mesh(mesh.quad_mesh)
--- examples/surface_constrained_smoothing.py
+++ examples/surface_constrained_smoothing.py
@@ -13,16 +13,16 @@ srf = RhinoSurface.from_selection()
 guids = rs.GetObjects('get meshes', filter = 32)
 
 for guid in guids:
-    mesh = CoarseQuadMesh.from_vertices_and_faces(*RhinoMesh.from_guid(guid).get_vertices_and_faces())
+    mesh = Mesh.from_vertices_and_faces(*RhinoMesh.from_guid(guid).get_vertices_and_faces())
     
-    mesh.collect_strips()
-    mesh.init_strip_density()
+    #mesh.collect_strips()
+    #mesh.init_strip_density()
     #mesh.set_strips_density(6)
-    mesh.set_strips_density_target(.5)
-    mesh.densification()
+    #mesh.set_strips_density_target(.5)
+    #mesh.densification()
     
-    constraints = automated_smoothing_surface_constraints(mesh.quad_mesh, srf)
-    constrained_smoothing(mesh.quad_mesh, kmax = 100, damping = .5, constraints = constraints, algorithm = 'area')
+    constraints = automated_smoothing_surface_constraints(mesh, srf)
+    constrained_smoothing(mesh, kmax = 75, damping = .5, constraints = constraints, algorithm = 'area')
     
-    new_guid = draw_mesh(mesh.quad_mesh)
-    rs.MoveObject(new_guid, [11,0,0])
+    new_guid = draw_mesh(mesh)
+    #rs.MoveObject(new_guid, [11,0,0])
