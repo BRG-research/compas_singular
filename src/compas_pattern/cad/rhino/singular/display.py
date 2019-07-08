62c1fa41fb413bb149fdd8847c45b1980f47b510
Robin Oval <oval@arch.ethz.ch>
2019-07-08 11:03:34 +0200

    wip singular tool

---
+++ src/compas_pattern/cad/rhino/singular/display.py
@@ -0,0 +1,23 @@
+import compas
+
+try:
+    import rhinoscriptsyntax as rs
+
+except ImportError:
+    compas.raise_if_ironpython()
+
+from compas_pattern.cad.rhino.draw import draw_mesh
+
+
+def change_display(singularity_mesh):
+
+	display_mesh = rs.GetString('display...', strings=['singularity_mesh', 'density_mesh', 'pattern'])
+
+	if display_mesh == 'singularity_mesh':
+		draw_mesh(singularity_mesh)
+
+	elif display_mesh == 'density_mesh':
+		draw_mesh(singularity_mesh.quad_mesh)
+
+	elif display_mesh == 'pattern':
+		draw_mesh(singularity_mesh.polygonal_mesh)
