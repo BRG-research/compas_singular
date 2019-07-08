c65f3b790913f504d2488c9f869f6689cfe05768
Robin Oval <oval@arch.ethz.ch>
2019-07-08 10:56:10 +0200

    cleaning

--- src/compas_pattern/datastructures/network/network.py
+++ src/compas_pattern/datastructures/network/network.py
@@ -5,14 +5,17 @@ __copyright__  = 'Copyright 2017, Block Research Group - ETH Zurich'
 __license__    = 'MIT License'
 __email__      = 'oval@arch.ethz.ch'
 
-__all__ = [
+__all__ = ['Network']
 
-]
+
+### TO BE PUSHED TO COMPAS ###
+
 
 class Network(Network):
 
 	def __init__(self):
 		super(Network, self).__init__()
+
 
 # ==============================================================================
 # Main
