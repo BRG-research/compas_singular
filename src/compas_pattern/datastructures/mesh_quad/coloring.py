525c381696d7de94596f7d1c0cda847e062235b4
Robin Oval <oval@arch.ethz.ch>
2019-07-08 11:00:10 +0200

    quad mesh element coloring

--- src/compas_pattern/datastructures/mesh_quad/coloring.py
+++ src/compas_pattern/datastructures/mesh_quad/coloring.py
@@ -1,18 +1,13 @@
-from compas_pattern.datastructures.network.network import Network
-from compas_pattern.datastructures.network.coloring import is_network_two_colorable
+from compas.topology import adjacency_from_edges
+from compas_pattern.topology.coloring import is_adjacency_two_colorable
 from compas.topology import vertex_coloring
 
-from compas.geometry import centroid_points
-
-__author__ = ['Robin Oval']
-__copyright__ = 'Copyright 2018, Block Research Group - ETH Zurich'
-__license__ = 'MIT License'
-__email__ = 'oval@arch.ethz.ch'
 
 __all__ = [
     'quad_mesh_strip_2_coloring',
     'quad_mesh_strip_n_coloring',
-    'quad_mesh_polyedge_2_coloring'
+    'quad_mesh_polyedge_2_coloring',
+    'quad_mesh_polyedge_n_coloring'
 ]
 
 
@@ -22,18 +17,17 @@ def quad_mesh_strip_2_coloring(quad_mesh):
     Parameters
     ----------
     quad_mesh : QuadMesh
-            A quad mesh.
+        A quad mesh.
 
     Returns
     -------
     dict, None
-            A dictionary with strip keys pointing to colors, if two-colorable.
-            None if not two-colorable.
-
+        A dictionary with strip keys pointing to colors, if two-colorable.
+        None if not two-colorable.
     """
 
-    quad_mesh.collect_strips()
-    return is_network_two_colorable(quad_mesh.strip_connectivity())
+    vertices, edges = quad_mesh.strip_graph()
+    return is_adjacency_two_colorable(adjacency_from_edges(edges))
 
 
 def quad_mesh_strip_n_coloring(quad_mesh):
@@ -42,43 +36,55 @@ def quad_mesh_strip_n_coloring(quad_mesh):
     Parameters
     ----------
     quad_mesh : QuadMesh
-            A quad mesh.
+        A quad mesh.
 
     Returns
     -------
     dict
-            A dictionary with strip keys pointing to colors.
-
+        A dictionary with strip keys pointing to colors.
     """
 
-    quad_mesh.collect_strips()
-    return vertex_coloring(quad_mesh.strip_connectivity().adjacency)
-
-
-def quad_mesh_polyedge_graph(quad_mesh):
-    polyedges = quad_mesh.polyedges()
-    polyedges = [tuple(polyedge) for polyedge in polyedges]
-
-    vertices = {i: centroid_points([quad_mesh.vertex_coordinates(vkey) for vkey in polyedge]) for i, polyedge in enumerate(polyedges)}
-    edges = []
-    for i, polyedge in enumerate(polyedges):
-        for vkey in polyedge:
-            if not quad_mesh.is_vertex_singular(vkey):
-                for j, polyedge_2 in enumerate(polyedges):
-                    if vkey in polyedge_2:
-                        edges.append((i, j))
-                        break
-    return Network.from_vertices_and_edges(vertices, edges)
+    vertices, edges = quad_mesh.strip_graph()
+    return vertex_coloring(adjacency_from_edges(edges))
 
 
 def quad_mesh_polyedge_2_coloring(quad_mesh):
-    network = quad_mesh_polyedge_graph(quad_mesh)
-    return is_network_two_colorable(network)
+    """Try to color the polyedges of a quad mesh with two colors only without overlapping polyedges with the same color.
+    Polyedges connected by their extremities, which are singularities, do not count as overlapping.
+
+    Parameters
+    ----------
+    quad_mesh : QuadMesh
+        A quad mesh.
+
+    Returns
+    -------
+    dict, None
+        A dictionary with polyedge keys pointing to colors, if two-colorable.
+        None if not two-colorable.
+    """
+
+    vertices, edges = quad_mesh.polyedge_graph()
+    return is_adjacency_two_colorable(adjacency_from_edges(edges))
 
 
 def quad_mesh_polyedge_n_coloring(quad_mesh):
-    network = quad_mesh_polyedge_graph(quad_mesh)
-    return vertex_coloring(network)
+    """Color the polyedges of a quad mesh with a minimum number of colors without overlapping polyedges with the same color.
+    Polyedges connected by their extremities, which are singularities, do not count as overlapping.
+
+    Parameters
+    ----------
+    quad_mesh : QuadMesh
+        A quad mesh.
+
+    Returns
+    -------
+    dict
+        A dictionary with polyedge keys pointing to colors.
+    """
+
+    vertices, edges = quad_mesh.polyedge_graph()
+    return vertex_coloring(adjacency_from_edges(edges))
 
 
 # ==============================================================================
@@ -88,3 +94,14 @@ def quad_mesh_polyedge_n_coloring(quad_mesh):
 if __name__ == '__main__':
 
     import compas
+    from compas_pattern.datastructures.mesh_quad.mesh_quad import QuadMesh
+
+    mesh = QuadMesh.from_obj(compas.get('faces.obj'))
+    
+    mesh.collect_strips()
+    mesh.collect_polyedges()
+
+    print(quad_mesh_strip_2_coloring(mesh))
+    print(quad_mesh_strip_n_coloring(mesh))
+    print(quad_mesh_polyedge_2_coloring(mesh))
+    print(quad_mesh_polyedge_n_coloring(mesh))
