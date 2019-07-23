1e5a36eb68d1a0ccfe6e189fcb9fc9bed57c2438
Robin Oval <oval@arch.ethz.ch>
2019-07-23 17:54:22 +0200

    mesh_uv_to_xyz moved to compas

--- src/compas_pattern/cad/rhino/objects/surface.py
+++ src/compas_pattern/cad/rhino/objects/surface.py
@@ -31,22 +31,6 @@ class RhinoSurface(RhinoSurface):
 	def __init__(self, guid):
 		super(RhinoSurface, self).__init__(guid) 
 
-	def mesh_uv_to_xyz(self, mesh):
-		"""Remap a mesh with UV coordinates to a surface based on its UV-parameterisation.
-		The third coordinate of the mesh vertices is discarded.
-
-		Parameters
-		----------
-		mesh : Mesh
-			A mesh.
-		"""
-
-		for vkey in mesh.vertices():
-			x, y, z = self.point_uv_to_xyz(mesh.vertex_coordinates(vkey)[:2])
-			mesh.vertex[vkey]['x'] = x
-			mesh.vertex[vkey]['y'] = y
-			mesh.vertex[vkey]['z'] = z
-
 
 # ==============================================================================
 # Main
