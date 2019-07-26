3a7207c674ce7b62e59b965ae47d84c7429bd57d
Robin Oval <oval@arch.ethz.ch>
2019-07-26 17:26:41 +0200

    obsolete draw_mesh

--- examples/rhino_surface_decomposition.py
+++ examples/rhino_surface_decomposition.py
@@ -1,6 +1,7 @@
 import rhinoscriptsyntax as rs
 from compas_pattern.algorithms.decomposition.algorithm import surface_decomposition
-from compas_pattern.cad.rhino.draw import draw_mesh
+
+from compas_rhino.artists import MeshArtist
 
 guids = rs.GetObjects('get surfaces', filter=8)
 crv_guids = rs.GetObjects('get curves', filter=4) or []
@@ -9,4 +10,4 @@ discretisation = rs.GetReal('discretisation value', 1, 0)
 for srf_guid in guids:
     decomposition = surface_decomposition(srf_guid, discretisation, crv_guids = crv_guids, pt_guids = pt_guids, output_delaunay=False, output_skeleton=False, output_mesh=True, output_polysurface=False)[0]
     rs.EnableRedraw(False)
-    draw_mesh(decomposition)
+    MeshArtist(decomposition).draw_mesh()
