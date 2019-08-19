f8ea2ea7bd4e7c39d06177a92aee347c0b4226fb
Robin Oval <oval@arch.ethz.ch>
2019-08-19 10:41:21 +0200

    update init

--- src/compas_pattern/datastructures/mesh/__init__.py
+++ src/compas_pattern/datastructures/mesh/__init__.py
@@ -1,4 +1,3 @@
 from compas_pattern.datastructures.mesh.mesh import *
 from compas_pattern.datastructures.mesh.operations import *
-from compas_pattern.datastructures.mesh.unweld import *
 from compas_pattern.datastructures.mesh.coloring import *
