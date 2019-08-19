88b5a43abaf144c58304b820e8a2c2f483b73ead
Robin Oval <oval@arch.ethz.ch>
2019-08-19 10:40:32 +0200

    fix init

--- src/compas_pattern/datastructures/mesh_quad/__init__.py
+++ src/compas_pattern/datastructures/mesh_quad/__init__.py
@@ -1,4 +1,2 @@
 from compas_pattern.datastructures.mesh_quad.mesh_quad import *
-from compas_pattern.datastructures.mesh_quad.grammar_pattern import *
-from compas_pattern.datastructures.mesh_quad.grammar_shape import *
 from compas_pattern.datastructures.mesh_quad.coloring import *
