ac601dd0f945eea8d68a8aae36a68f365ed2eb50
Robin Oval <oval@arch.ethz.ch>
2019-08-19 10:35:48 +0200

    arrays in line, rectangle, circle and spiral

--- src/compas_pattern/geometry/arrays.py
+++ src/compas_pattern/geometry/arrays.py
@@ -1,16 +1,37 @@
+from math import pi
+from compas.geometry import circle_evaluate
+from compas.geometry import archimedean_spiral_evaluate
 from compas.geometry import add_vectors
 
-__all__ = ['rectangular_array']
+
+__all__ = [
+	'line_array',
+	'rectangular_array',
+	'circular_array',
+	'spiral_array'
+	]
+
+
+def line_array(n, d, anchor=[0.0, 0.0, 0.0]):
+	return [add_vectors(anchor, [i * d, 0.0, 0.0]) for i in range(n)]
 
 
 def rectangular_array(nx, ny, dx, dy, anchor=[0.0, 0.0, 0.0]):
+	return [add_vectors(anchor, [x * dx, y * dy, 0.0]) for y in range(ny) for x in range(nx)]
 
-	pts = []
-	for y in range(ny):
-		for x in range(nx):
-			pts.append(add_vectors(anchor, [x * dx, y * dy, 0.0]))
 
-	return pts
+def circular_array(n, r, anchor=[0.0, 0.0, 0.0]):
+	return [add_vectors(anchor, circle_evaluate(2 * pi * float(i) / float(n), r)) for i in range(n)]
+
+
+def spiral_array(n, d, anchor=[0.0, 0.0, 0.0]):
+	# spiral parameters set to respect d spacing between consecutive points and consecutive spiral elements
+	a, b = 0, d / (2 * pi)
+	ts = [pi * b]
+	for i in range(n):
+		ts.append((2 * d / b + ts[-1] ** 2) ** .5)
+	return [add_vectors(anchor, archimedean_spiral_evaluate(t, a, b, 0)) for t in ts]
+
 
 # ==============================================================================
 # Main
@@ -19,3 +40,4 @@ def rectangular_array(nx, ny, dx, dy, anchor=[0.0, 0.0, 0.0]):
 if __name__ == '__main__':
 
 	print(rectangular_array(4, 2, 10.0, 0.5, anchor=[1.0, -1.0, 0.0]))
+	print(spiral_array(15, 2, anchor=[1.0, -1.0, 0.0]))
