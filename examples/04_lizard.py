3e8cd3c358e8b681df63ca1d75b6efca2de179a6
Robin Oval <rpho2@cam.ac.uk>
2020-12-18 10:29:01 +0000

    example file for lizard

---
+++ examples/04_lizard.py
@@ -0,0 +1,35 @@
+from compas_singular.datastructures import QuadMesh
+from compas_singular.datastructures.lizard import Lizard
+from compas.datastructures import mesh_smooth_centroid
+from compas_plotters.meshplotter import MeshPlotter
+
+
+# input mesh - standard 3 x 3 grid
+vertices = [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [2.0, 0.0, 0.0], [0.0, 1.0, 0.0], [1.0, 1.0, 0.0], [2.0, 1.0, 0.0], [0.0, 2.0, 0.0], [1.0, 2.0, 0.0], [2.0, 2.0, 0.0]]
+faces = [[0, 1, 4, 3], [1, 2, 5, 4], [3, 4, 7, 6], [4, 5, 8, 7]]
+mesh0 = QuadMesh.from_vertices_and_faces(vertices, faces)
+mesh0.collect_strips()
+
+# # plot input quad mesh
+plotter = MeshPlotter(mesh0, figsize=(5, 5))
+plotter.draw_edges()
+plotter.draw_vertices(radius=.05)
+plotter.draw_faces()
+plotter.show()
+
+# edit topology with lizard
+mesh = mesh0.copy()
+lizard = Lizard(mesh)
+lizard.initiate()
+string = 'ata' # try: ata, attta, d, atttad, attpptta ...
+lizard.from_string_to_rules(string)
+
+# geometrical processing
+mesh_smooth_centroid(mesh, kmax=1, damping=0.5)
+
+# plot output quad mesh
+plotter = MeshPlotter(mesh, figsize=(5, 5))
+plotter.draw_edges()
+plotter.draw_vertices(radius=.05)
+plotter.draw_faces()
+plotter.show()
