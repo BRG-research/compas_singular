54e0ede0c768699567894d5e9a5796710cdd42f7
brgcode <vanmelet@ethz.ch>
2021-04-23 16:45:20 +0200

    fix bumpversion quotes

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_singular",
-    version="version='0.1.1'",
+    version="0.1.1",
     description="topology finding of compas_singularities in quad meshes",
     long_description=long_description,
     long_description_content_type="text/markdown",
